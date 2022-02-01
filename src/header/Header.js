25356ad9859c655f4782f72a8b47296be33e9973
ivankvach <ivankvach6@gmail.com>
2022-02-01 11:48:08 +0200

    setState

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,9 +1,20 @@
+import {useState} from 'react';
 import './Header.css'
 
 function Header(props) {
+    const [counter, setCounter] = useState(0)
+    
+    function up() {
+        setCounter(counter + 1)
+    }
+    function down() {
+        setCounter(counter - 1)
+    }
     return(
         <div className="Header-site-header">
-            <h1>{props.sitename}</h1>
+            <h1>{props.sitename}№{counter}</h1>
+            <button onClick={up}>Up</button>
+            <button onClick={down}>Down</button>
             <h2>{props.sitedescription}</h2>
         </div>
     );
